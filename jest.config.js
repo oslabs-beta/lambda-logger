d177b3d7dd277b99b7fb14008a9c10fd4afc65fd
nickmasonswe <nickmasonswe@gmail.com>
2023-11-20 15:29:23 -0500

    add jest testing for back end with minimal config in jest.config.js + run test script to package. json. working

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testEnvironment: 'node', // or 'jsdom' for browser environments
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.jsx?$',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+};
