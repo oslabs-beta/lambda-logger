5ebe8c8ff6cc60208c063503b67b10ed837181c2
LClarkson <l.j.clarkson@gmail.com>
2023-11-24 10:56:33 -0800

    first server.js test suite

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,3 +1,22 @@
-test('example test', () => {
-  expect(true).toBe(true);
+const request = require('supertest');
+const server = require('../server/server.js');
+
+afterAll(() => {
+  server.close()
+})
+
+describe('\n Server Route Tests \n', () => {
+
+  describe('Credentials Page Route', () => {
+
+    it('should respond with status 200 OK', async () => {
+      const response = await request(server).get('/');
+      expect(response.status).toBe(200);
+    });
+    
+    it('should respond with content-type text/html', async () => {
+      const response = await request(server).get('/');
+      expect(response.headers['content-type']).toContain('text/html');
+    });
+  });
 });
