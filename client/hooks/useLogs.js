8afb81982900128c22385cd3d01eba04b468eeac
Luke Clarkson <l.j.clarkson@gmail.com>
2023-11-28 14:36:26 -0800

    eslint fixes

--- client/hooks/useLogs.js
+++ client/hooks/useLogs.js
@@ -1,5 +1,5 @@
 // hooks/useLogs.js
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect, useCallback } from 'react';
 
 function useLogs(
   accessKey,
@@ -8,26 +8,26 @@ function useLogs(
   selectedLogGroup,
   selectedLogStream
 ) {
-  const [logs, setLogs] = useState("NO LOGS REQUESTED YET");
+  const [logs, setLogs] = useState('NO LOGS REQUESTED YET');
   const fetchLogs = useCallback(async () => {
-    const url = "http://localhost:8080/logs";
+    const url = 'http://localhost:8080/logs';
 
     try {
       const response = await fetch(url, {
-        method: "GET", // Assuming the endpoint is expecting a GET request
+        method: 'GET', // Assuming the endpoint is expecting a GET request
         headers: {
-          "Content-Type": "application/json",
-          "Access-Key": encodeURIComponent(accessKey),
-          "Secret-Key": encodeURIComponent(secretKey),
-          "AWS-Region": encodeURIComponent(region),
-          "Log-Group": encodeURIComponent(selectedLogGroup),
-          "Log-Stream": encodeURIComponent(selectedLogStream),
+          'Content-Type': 'application/json',
+          'Access-Key': encodeURIComponent(accessKey),
+          'Secret-Key': encodeURIComponent(secretKey),
+          'AWS-Region': encodeURIComponent(region),
+          'Log-Group': encodeURIComponent(selectedLogGroup),
+          'Log-Stream': encodeURIComponent(selectedLogStream),
         },
       });
       const data = await response.json();
       setLogs(data);
     } catch (error) {
-      console.error("Failed to fetch logs:", error);
+      console.error('Failed to fetch logs:', error);
     }
   }, [accessKey, secretKey, region, selectedLogGroup, selectedLogStream]);
 
