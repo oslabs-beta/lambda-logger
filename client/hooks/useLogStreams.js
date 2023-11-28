d2347a26e9d337898458c8cb480e3733c96c18ea
Junglepanda69 <Conrady82@gmail.com>
2023-11-28 09:16:08 -0800

    Create Hooks

---
+++ client/hooks/useLogStreams.js
@@ -0,0 +1,43 @@
+// hooks/useLogStreams.js
+import { useState, useEffect, useCallback } from "react";
+
+function useLogStreams(accessKey, secretKey, region, selectedLogGroup) {
+  const [logStreams, setLogStreams] = useState([]);
+  const [selectedLogStream, setSelectedLogStream] = useState("");
+
+  const fetchLogStreams = useCallback(async () => {
+    const url = "http://localhost:8080/logstreams";
+
+    try {
+      const response = await fetch(url, {
+        method: "GET", // Assuming the endpoint is expecting a GET request
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Key": encodeURIComponent(accessKey),
+          "Secret-Key": encodeURIComponent(secretKey),
+          "AWS-Region": encodeURIComponent(region),
+          "Log-Group": encodeURIComponent(selectedLogGroup),
+        },
+      });
+      const data = await response.json();
+      setLogStreams(data);
+    } catch (error) {
+      console.error("Failed to fetch log streams:", error);
+    }
+  }, [accessKey, secretKey, region, selectedLogGroup]);
+
+  useEffect(() => {
+    if (selectedLogGroup) {
+      fetchLogStreams();
+    }
+  }, [selectedLogGroup, fetchLogStreams]);
+
+  return {
+    logStreams,
+    selectedLogStream,
+    setSelectedLogStream,
+    fetchLogStreams,
+  };
+}
+
+export default useLogStreams;
---
+++ client/hooks/useThemeButton.js
@@ -0,0 +1,24 @@
+import { useState, useEffect, useCallback } from "react";
+import {
+  stackoverflowDark,
+  stackoverflowLight,
+} from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+function useThemeButton() {
+  const [theme, setTheme] = useState(stackoverflowDark);
+  const [themeButton, setThemeButton] = useState("Light Mode");
+  function handleThemeButtonClick() {
+    theme === stackoverflowDark
+      ? (setTheme(stackoverflowLight), setThemeButton("Dark Mode"))
+      : (setTheme(stackoverflowDark), setThemeButton("Light Mode"));
+  }
+  return {
+    theme,
+    setTheme,
+    themeButton,
+    setThemeButton,
+    handleThemeButtonClick,
+  };
+}
+
+export default useThemeButton;
