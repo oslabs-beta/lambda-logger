3aade0e67bed2a06104b7fe50279914305fd6621
Junglepanda69 <Conrady82@gmail.com>
2023-11-30 10:53:14 -0800

    Link feature section to App

--- client/components/Splash/FeatureSection.jsx
+++ client/components/Splash/FeatureSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from '../../src/styles/FeatureSection.module.css'; // Adjust the path as needed
 
 export default function FeatureSection() {
@@ -8,7 +9,9 @@ export default function FeatureSection() {
         <h2>Searchable and Readable</h2>
         <p>Lambda Logger gives you the power to quickly read and search <br/> any log file you need straight from our seamless, secure dashboard. <br/>
         Lightweight and optimized so you can finally <br/>live de-bug your Lambdas in peace.</p>
-        <button className={styles.HoverButton}>Learn More</button>
+        <Link to="/credentials">
+          <button className={styles.HoverButton}>Try Now</button>
+        </Link>
       </div>
     </div>
   );
