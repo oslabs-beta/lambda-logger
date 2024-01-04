68302af16019ff9797b0b4d3a8eb3b46ff37d6ae
HoangDang <hoangdang91768@gmail.com>
2024-01-04 07:19:43 -0900

    trying to fix npm run dev vs. npm start problem

--- server/server.js
+++ server/server.js
@@ -9,12 +9,8 @@ const logRoutes = require('./routes/logRoutes');
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-if (process.env.NODE_ENV === 'production') {
-  // app.use(express.static(path.resolve(__dirname, '../build')));
-  app.use(express.static(path.resolve(__dirname, '../client')));
-} else {
-  app.use(express.static(path.resolve(__dirname, '../client')));
-}
+app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(express.static(path.resolve(__dirname, '../client')));
 
 // Routes
 app.use('/credentials', logRoutes);
